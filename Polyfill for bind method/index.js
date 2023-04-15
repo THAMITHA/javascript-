592e1b8cb4596c8458bd9ad0bf1227a47b91d538
Thamitha <thamitharajput@gmail.com>
2023-04-15 18:01:53 +0530

    add polyfill

--- Polyfill for bind method/index.js
+++ Polyfill for bind method/index.js
@@ -1,24 +1,34 @@
-let name={
-    firstname:"Thamitha",
-    lastname:"Rajput"
-}
+let name = {
+  firstname: "Thamitha",
+  lastname: "Rajput",
+};
 
-let printName=function(hometown,state,country){
-    console.log(this.firstname+" "+this.lastname+" "+hometown+" "+state+" "+country);
-}
+let printName = function (hometown, state, country) {
+  console.log(
+    this.firstname +
+      " " +
+      this.lastname +
+      " " +
+      hometown +
+      " " +
+      state +
+      " " +
+      country
+  );
+};
 
-let printMyName=printName.bind(name,"mysore","karnataka");
+let printMyName = printName.bind(name, "mysore", "karnataka");
 printMyName("India");
 
-//implmentation
+//implementation
 
-Function.prototype.myBind=function(...args){
-    let obj=this;
-    let params=args.slice(1);
-    return function(...args2){
-        obj.apply(args[0],[...params,...args2]);
-    }
-}
+Function.prototype.myBind = function (...args) {
+  let obj = this;
+  let params = args.slice(1);
+  return function (...args2) {
+    obj.apply(args[0], [...params, ...args2]);
+  };
+};
 
-let printMyName2=printName.myBind(name,"mysore","karnataka");
+let printMyName2 = printName.myBind(name, "mysore", "karnataka");
 printMyName2("India");
