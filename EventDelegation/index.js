89db23d9a8ae31f7ef1f5ff92de27a61ee0dd317
Thamitha <thamitharajput@gmail.com>
2023-04-16 12:35:21 +0530

    add event propogation

--- EventDelegation/index.js
+++ EventDelegation/index.js
@@ -1,6 +1,13 @@
-document.querySelector("#category").addEventListener('click', (e) => {
-  console.log(e.target.id);
-  if(e.target.tagName=='LI'){
-    window.location.href="/"+e.target.id;
-  }
-});
+// document.querySelector("#category").addEventListener('click', (e) => {
+//   console.log(e.target.id);
+//   if(e.target.tagName=='LI'){
+//     window.location.href="/"+e.target.id;
+//   }
+// });
+
+document.querySelector('#form').addEventListener('keyup', (e)=>{
+    if(e.target.dataset.uppercase !=undefined){
+        console.log(e);
+        e.target.value=e.target.value.toUpperCase();
+    }
+})
